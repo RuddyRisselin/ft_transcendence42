ab6b8b07263ab8acdb525467fb47351866820615
Malo Lefort <mlefort@bess-f3r6s6.clusters.42paris.fr>
2025-04-03 18:09:14 +0200

    refactor(typescript): add layout.ts delete button.ts && form.ts

--- frontend/src/components/layout.ts
+++ frontend/src/components/layout.ts
@@ -1,10 +1,10 @@
 
 export default function Layout(pageContent: HTMLElement) {
-  const container = document.createElement("div");
+  const container: HTMLDivElement = document.createElement("div");
   container.className = "min-h-screen flex flex-col bg-gray-900 text-white";
 
   // Contenu principal
-  const contentWrapper = document.createElement("div");
+  const contentWrapper: HTMLDivElement = document.createElement("div");
   contentWrapper.className = "flex flex-col items-center justify-center min-h-screen mt-16 px-4 w-full";
 
   contentWrapper.appendChild(pageContent);
